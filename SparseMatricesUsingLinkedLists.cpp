1fc59c1e48981f316a3538617ce587d80c261231
HRKagdi <53858937+HRKagdi@users.noreply.github.com>
2021-09-11 20:07:54 +0530

    Update SparseMatricesUsingLinkedLists.cpp

--- SparseMatricesUsingLinkedLists.cpp
+++ SparseMatricesUsingLinkedLists.cpp
@@ -1,144 +1,187 @@
-#include<iostream> 
+#include<iostream>
 using namespace std;
 template <typename T>
-class Node{
-	public:
-	int x;
-	int y;
-	T data;
-	Node * next;
-};
-template <typename T>
 class SparseMatrices{
-	Node<T> * matrix;
-	int capacity;
+	T ** matrix=new T *[3];
 	int size;
-	public:
+	int capacity;
+	public :
 	SparseMatrices(int size){
 		this->size=size;
-		this->capacity=capacity;
-		this->matrix=new Node<T>();
+		for(int i=0;i<3;i++){
+			matrix[i]=new T[size];
+		}
+		this->capacity=0;
+	}
+	
+	void addElement(int r,int c,T value){
+		matrix[0][capacity]=r;
+		matrix[1][capacity]=c;
+		matrix[2][capacity]=value;
+		
+		capacity++;
 	}
 	
 	void transpose(){
-		Node<T> * temp=this->matrix;
-		while(temp!=NULL){
-			int t=temp->x;
-			temp->x=temp->y;
-			temp->y=t;
-			temp=temp->next;
+		for(int i=0;i<size;i++){
+			T temp=this->matrix[0][i];
+			this->matrix[0][i]=this->matrix[1][i];
+			this->matrix[1][i]=temp;
 		}
 	}
 	
-	void print(){
-		Node<T> * temp=this->matrix;
-		while(temp->next!=NULL && temp->data!=0){
-			cout<<temp->x<<" "<<temp->y<<" "<<temp->data<<endl;
-			temp=temp->next;
-		}	
-	}
-	
-	void addElement(int x,int y,T data){
-		Node<T> * t=new Node<T>;
-		t->x=x; t->y=y; t->data=data;
-		t->next=this->matrix;
-		this->matrix=t;
-		this->capacity++;
-	}
-	
-	void Add(SparseMatrices<T>* mat){
-		Node<T> * t1=this->matrix; Node<T> * t2=mat->matrix;
-		while(t1!=NULL){
-			int x=t1->x;
-			int y=t1->y;
-			t2=mat->matrix;
-			while(t2!=NULL){
-				if(x==t2->x && y==t2->y){
-					t1->data+=t2->data;
+	void Add(SparseMatrices<T> *mat){
+		for(int i=0;i<this->size;i++){
+			int x=this->matrix[0][i];
+			int y=this->matrix[1][i];
+			bool b=false;
+			for(int j=0;j<this->size;j++){
+				if(x==mat->matrix[0][j] && y==mat->matrix[1][j]){
+					this->matrix[0][i]=x;
+					this->matrix[1][i]=y;
+					this->matrix[2][i]=this->matrix[2][i]+mat->matrix[2][j];
 				}
-				t2=t2->next;
 			}
-			t1=t1->next;
 		}
-		while(t2!=NULL){
-			int x=t2->x; int y=t2->y;
-			t1=this->matrix; bool b=false;
-			while(t1!=NULL){
-				if(x==t1->x && y==t1->y){
+		
+		for(int i=0;i<this->size;i++){
+			int x=mat->matrix[0][i];
+			int y=mat->matrix[1][i];
+			bool b=false;
+			for(int j=0;j<this->size;j++){
+				if(x==this->matrix[0][j] && y==this->matrix[1][j]){
 					b=true;
 				}
-				t1=t1->next;
 			}
 			if(!b){
-				this->addElement(x,y,t2->data);
+				this->addElement(x,y,mat->matrix[2][i]);
 			}
-			t2=t2->next;
+		}	
+	}
+	void print(){
+		for(int i=0;i<this->capacity;i++){
+			cout<<this->matrix[0][i]<<" "<<this->matrix[1][i]
+			<<" "<<this->matrix[2][i]<<endl; 
 		}
 	}
-	
-	void Multiply(SparseMatrices<T>* mat){
-  		SparseMatrices<T>* res=new SparseMatrices<T>(this->capacity+mat->capacity);
-  		Node<T> *t1,*t2,*temp;
-  		temp=new Node<T>();
-  		t1=this->matrix->next;
- 		 while(t1!=NULL)
-  		{
-   			t2=mat->matrix->next;
-   			while(t2->next!=NULL)
-   			{
-    				if(t1->y==t2->x)
-    				{
-    					temp->x=t1->x;
-    					temp->y=t2->y;
-    					temp->data=(t1->data)*(t2->data);
-   					temp->next=NULL;
-    					res->addElement(temp->x,temp->y,temp->data);
-    				}
-    				t2=t2->next;
-   			}
-   			if(t2->next==NULL)
-   			{
-    				if(t1->y==t2->x)
-    				{
-    					temp->x=t1->x;
-    					temp->y=t2->y;
-    					temp->data=(t1->data)*(t2->data);
-    					temp->next=NULL;
-    					res->addElement(temp->x,temp->y,temp->data);
-    				}
-   			}
-   			t1=t1->next;
-  		}
+	void Multiply(SparseMatrices<T> * mat){
+	if (this->size != mat->size)
+        {
+            cout << "Can't multiply, Invalid dimensions";
+            return;
+        }
 
-  		res->matrix->x=this->matrix->x;
-  		res->matrix->y=mat->matrix->y;
-  		res->print(); 
-  		//cout<<res->capacity;
-	
-	}	
+        mat->transpose();
+        int apos, bpos;
+  
+        SparseMatrices<T>* result=new SparseMatrices(this->capacity+mat->capacity);
+  
+        for (apos = 0; apos < this->capacity;)
+        {
+            T r = this->matrix[0][apos];
+  
+            for (bpos = 0; bpos < mat->capacity;)
+            {
+                T c = mat->matrix[0][bpos];
+  
+                T tempa = apos;
+                T tempb = bpos;
+  
+                T sum = 0;
+  
+                while (tempa < this->capacity && this->matrix[0][tempa] == r &&
+                       tempb < mat->capacity && mat->matrix[0][tempb] == c)
+                {
+                    if (this->matrix[1][tempa] < mat->matrix[1][tempb])
+  
+                        tempa++;
+  
+                    else if (this->matrix[1][tempa] > mat->matrix[1][tempb])
+                        tempb++;
+                    else
+                        sum += this->matrix[2][tempa++] * 
+                             mat->matrix[2][tempb++];
+                }
+  
+
+                if (sum != 0)
+                    result->addElement(r, c, sum);
+  
+                while (bpos < mat->capacity && 
+                       mat->matrix[0][bpos] == c)
+ 
+                    bpos++;
+            }
+            while (apos < this->capacity && this->matrix[0][apos] == r)
+ 
+                apos++;
+        }
+        result->print();
+	}
 };
 int main(){
-	cout<<"Enter the matrices"<<endl;
-	cout<<"Enter the size"<<endl;
-	int size; cin>>size; 
-	cout<<"Enter the number of elements to be added"<<endl;
-	int z; cin>>z;
-	SparseMatrices<int> * mat1=new SparseMatrices<int>(size);
-	for(int i=0;i<z;i++){
-		int x,y,value; cin>>x>>y>>value;
-		mat1->addElement(x,y,value);
+	while(true){
+		cout<<"Enter your choice"<<endl;
+		cout<<"Enter 1 to find transpose of a matrix"<<endl;
+		cout<<"Enter 2 to add two matrices"<<endl;
+		cout<<"Enter 3 to multiply two matrices"<<endl;
+		cout<<"Enter 4 to exit"<<endl;
+		int choice; cin>>choice;
+		if(choice==4) return 0;
+		else if(choice==1){
+			cout<<"Enter the size"<<endl;
+			int size; cin>>size; 
+			cout<<"Enter the number of elements to be added"<<endl;
+			int z; cin>>z;
+			SparseMatrices<int> * mat1=new SparseMatrices<int>(size);
+			for(int i=0;i<z;i++){
+				int x,y,value;cin>>x>>y>>value;
+				mat1->addElement(x,y,value);
+			}
+			mat1->transpose();
+			mat1->print();
+		}
+		else if(choice==2){
+			cout<<"Enter the size"<<endl;
+			int size; cin>>size; 
+			cout<<"Enter the number of elements to be added"<<endl;
+			int z; cin>>z;
+			SparseMatrices<int> * mat1=new SparseMatrices<int>(size);
+			for(int i=0;i<z;i++){
+				int x,y,value;cin>>x>>y>>value;
+				mat1->addElement(x,y,value);
+			}
+			cout<<"Enter second matric of size "<<size<<endl;
+			cout<<"Enter number of elements to be added"<<endl;
+			int num; cin>>num;
+			SparseMatrices<int> * mat2=new SparseMatrices<int>(size);
+			for(int i=0;i<num;i++){
+				int x,y,value; cin>>x>>y>>value;
+				mat2->addElement(x,y,value);
+			}
+			mat1->Add(mat2);
+			mat1->print();
+		}
+		else if(choice==3){
+			cout<<"Enter the size"<<endl;
+			int size; cin>>size; 
+			cout<<"Enter the number of elements to be added"<<endl;
+			int z; cin>>z;
+			SparseMatrices<int> * mat1=new SparseMatrices<int>(size);
+			for(int i=0;i<z;i++){
+				int x,y,value;cin>>x>>y>>value;
+				mat1->addElement(x,y,value);
+			}
+			cout<<"Enter second matric of size "<<size<<endl;
+			cout<<"Enter number of elements to be added"<<endl;
+			int num; cin>>num;
+			SparseMatrices<int> * mat2=new SparseMatrices<int>(size);
+			for(int i=0;i<num;i++){
+				int x,y,value; cin>>x>>y>>value;
+				mat2->addElement(x,y,value);
+			}
+			mat1->Multiply(mat2);
+		}
 	}
-	cout<<"Enter second matric of size "<<size<<endl;
-	cout<<"Enter number of elements to be added"<<endl;
-	int num; cin>>num;
-	SparseMatrices<int> * mat2=new SparseMatrices<int>(size);
-	for(int i=0;i<num;i++){
-		int x,y,value; cin>>x>>y>>value;
-		mat2->addElement(x,y,value);
-	}
-	mat1->Multiply(mat2);
-	//mat1->Add(mat2);
-	//mat1->transpose();
-	//mat1->print();
-	return 0;	
-}
+	return 0;
+}	
